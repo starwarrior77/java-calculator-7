755d9e8918c6eea33a55ec2dff04dece1cfbc932
ksw567590 <ksw567590@gmail.com>
2024-10-18 14:19:05 +0900

    feat: 빈 문자열 또는 NULL입력에 대한 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,23 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        try {
+            int result = calculate(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static int calculate(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+        // 이후 코드는 다음 커밋에서 추가
     }
 }
